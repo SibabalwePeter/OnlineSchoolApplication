7df66154c3776949632429ac9fc4bf7a347eb4ee
Sibabalwe <sibabalwepeter2848@gmail.com>
2021-02-18 12:50:34 -0800

    Learner Details

--- src/components/Learner/ApplicationSteps/PersonalDetails.tsx
+++ src/components/Learner/ApplicationSteps/PersonalDetails.tsx
@@ -1,17 +1,35 @@
 import * as React from 'react';
 import { Container } from 'reactstrap';
+import TextField from '@material-ui/core/TextField';
 
-class PersonalDetails extends React.PureComponent {
 
+
+export default class PersonalDetails extends React.PureComponent {
+    
     public render() {
         return (
             <Container>
-                <h1>Personal</h1>
+                <form  noValidate autoComplete="off">
+                    <div>
+                        <TextField className="d-xl m-4 col-4" label="First Name" />
+                        <TextField className="d-xl m-4 col-4" label="Middle Name" />
+                        <TextField className="d-xl m-4 col-4" label="Second Name" />
+                        <TextField className="d-xl m-4 col-4" label="ID Number" />
+                    </div>
+                    <div>
+                        <TextField className="d-xl m-4 col-4" label="Email Address" />
+                        <TextField className="d-xl m-4 col-4" label="Contact Number" />
+                        <TextField className="d-xl m-4 col-4" label="Contact Number 2" />
+                        <TextField className="d-xl m-4 col-4" label="Home Address" />
+                        <div className="input-group m-4 col-4">
+                            <div className="custom-file">
+                                <input type="file" className="custom-file-input d-xl" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01"/>
+                                <label className="custom-file-label" htmlFor="inputGroupFile01">Choose file</label>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </Container>
         )
     }
 }
-
-
-
-export default PersonalDetails
