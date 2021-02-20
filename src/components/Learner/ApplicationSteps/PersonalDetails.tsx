6dbba3c0c03b181a984a583c1c6d2dd1322880c0
Sibabalwe <sibabalwepeter2848@gmail.com>
2021-02-20 06:48:36 -0800

    PersonalDetails

--- src/components/Learner/ApplicationSteps/PersonalDetails.tsx
+++ src/components/Learner/ApplicationSteps/PersonalDetails.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Container } from 'reactstrap';
-import TextField from '@material-ui/core/TextField';
-
+import { FormControl, InputLabel, MenuItem, Select, TextField,   } from '@material-ui/core';
 
 
 export default class PersonalDetails extends React.PureComponent {
@@ -11,20 +10,31 @@ export default class PersonalDetails extends React.PureComponent {
             <Container>
                 <form  noValidate autoComplete="off">
                     <div>
-                        <TextField className="d-xl m-4 col-4" label="First Name" />
-                        <TextField className="d-xl m-4 col-4" label="Middle Name" />
-                        <TextField className="d-xl m-4 col-4" label="Second Name" />
-                        <TextField className="d-xl m-4 col-4" label="ID Number" />
+                        <FormControl className="d-xl m-4 col-1">
+                            <InputLabel id="genderLbl">Age</InputLabel>
+                            <Select
+                                labelId="genderLbl"
+                                id="gender"
+                                value="Male"
+                            >
+                                <MenuItem value="Male">Male</MenuItem>
+                                <MenuItem value="Female">Female</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <TextField className="d-xl m-4 col-3" label="First Name" />
+                        <TextField className="d-xl m-4 col-3" label="Middle Name" />
+                        <TextField className="d-xl m-4 col-3" label="Second Name" />
+                        <TextField className="d-xl m-4 col-3" label="ID Number" />
                     </div>
                     <div>
-                        <TextField className="d-xl m-4 col-4" label="Email Address" />
-                        <TextField className="d-xl m-4 col-4" label="Contact Number" />
-                        <TextField className="d-xl m-4 col-4" label="Contact Number 2" />
+                        <TextField className="d-xl m-4 col-2" label="Email Address" />
+                        <TextField className="d-xl m-4 col-2" label="Contact Number" />
+                        <TextField className="d-xl m-4 col-2" label="Contact Number 2" />
                         <TextField className="d-xl m-4 col-4" label="Home Address" />
-                        <div className="input-group m-4 col-4">
+                        <div className="input-group m-4 col-2">
                             <div className="custom-file">
                                 <input type="file" className="custom-file-input d-xl" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01"/>
-                                <label className="custom-file-label" htmlFor="inputGroupFile01">Choose file</label>
+                                <label className="custom-file-label" htmlFor="inputGroupFile01">Choose your image</label>
                             </div>
                         </div>
                     </div>
