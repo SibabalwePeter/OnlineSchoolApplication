af651c2b017f95cdcc48d0b25ed7fcae1458545a
Sibabalwe <sibabalwepeter2848@gmail.com>
2021-02-23 07:36:52 -0800

    learner details

--- src/components/Learner/ApplicationSteps/Form.tsx
+++ src/components/Learner/ApplicationSteps/Form.tsx
@@ -1,39 +1,164 @@
 import * as React from 'react';
 import { Container } from 'reactstrap';
-import { FormControl, InputLabel, MenuItem, Select, TextField,   } from '@material-ui/core';
+import { FormControl, FormControlLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, TextField,   } from '@material-ui/core';
 import * as Application from '../../../store/ApplicationForm';
 
 export const Learner=()=>{
     return (
         <Container>
             <form  noValidate autoComplete="off">
-                <div>
-                    <FormControl className="d-xl m-4 col-1">
-                        <InputLabel id="genderLbl">Gender</InputLabel>
-                        <Select
-                            labelId="genderLbl"
-                            id="gender"
-                            value="Male"
-                        >
-                            <MenuItem value="Male">Male</MenuItem>
-                            <MenuItem value="Female">Female</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <TextField className="d-xl m-4 col-3" label="First Name" />
-                    <TextField className="d-xl m-4 col-3" label="Middle Name" />
-                    <TextField className="d-xl m-4 col-3" label="Second Name" />
-                    <TextField className="d-xl m-4 col-3" label="ID Number" />
-                </div>
-                <div>
-                    <TextField className="d-xl m-4 col-2" label="Email Address" />
-                    <TextField className="d-xl m-4 col-2" label="Contact Number" />
-                    <TextField className="d-xl m-4 col-2" label="Contact Number 2" />
-                    <TextField className="d-xl m-4 col-4" label="Home Address" />
-                    <div className="input-group m-4 col-2">
-                        <div className="custom-file">
-                            <input type="file" className="custom-file-input d-xl" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01"/>
-                            <label className="custom-file-label" htmlFor="inputGroupFile01">Choose your image</label>
-                        </div>
+                <FormControl className="d-xl m-4 col-1">
+                    <InputLabel id="genderLbl">Gender</InputLabel>
+                    <Select
+                        labelId="genderLbl"
+                        id="gender"
+                        value="Male"
+                    >
+                        <MenuItem value="Male">Male</MenuItem>
+                        <MenuItem value="Female">Female</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField className="d-xl m-4 col-3" label="First Name" />
+                <TextField className="d-xl m-4 col-3" label="Middle Name" />
+                <TextField className="d-xl m-4 col-3" label="Second Name" />
+                <TextField className="d-xl m-4 col-3" label="ID Number" />
+                <TextField className="d-xl m-4 col-3" label="Passport Number" />
+                <FormControl className="d-xl m-4 col-1">
+                    <InputLabel id="genderLbl">Population Group</InputLabel>
+                    <Select
+                        labelId="genderLbl"
+                        id="gender"
+                        value="Male"
+                    >
+                        <MenuItem value="african">African</MenuItem>
+                        <MenuItem value="white">White</MenuItem>
+                        <MenuItem value="indian">Indian</MenuItem>
+                        <MenuItem value="colourde">Colourde</MenuItem>
+                        <MenuItem value="colourde">Other</MenuItem>
+                    </Select>
+                </FormControl>
+                <FormControl className="d-xl m-4 col-1">
+                    <InputLabel id="genderLbl">Home Language</InputLabel>
+                    <Select
+                        labelId="genderLbl"
+                        id="gender"
+                        value="Male"
+                    >
+                        <MenuItem value="african">IsiXhosa</MenuItem>
+                        <MenuItem value="white">English</MenuItem>
+                        <MenuItem value="indian">IsiZulu</MenuItem>
+                        <MenuItem value="colourde">Afrikaans</MenuItem>
+                        <MenuItem value="colourde">Other</MenuItem>
+                    </Select>
+                </FormControl>
+                <FormControl className="d-xl m-4 col-1">
+                    <InputLabel id="genderLbl">Language of Instructions</InputLabel>
+                    <Select
+                        labelId="genderLbl"
+                        id="gender"
+                        value="Male"
+                    >
+                        <MenuItem value="african">IsiXhosa</MenuItem>
+                        <MenuItem value="white">English</MenuItem>
+                        <MenuItem value="indian">IsiZulu</MenuItem>
+                        <MenuItem value="colourde">Afrikaans</MenuItem>
+                        <MenuItem value="colourde">Other</MenuItem>
+                    </Select>
+                </FormControl>
+                <InputLabel id="genderLbl">Boarding Learner? </InputLabel>
+                <RadioGroup aria-label="gender" name="gender1" value="yes">
+                    <FormControlLabel value="female" control={<Radio />} label="No" />
+                    <FormControlLabel value="male" control={<Radio />} label="Yes" />
+                </RadioGroup>
+                <TextField className="d-xl m-4 col-3" label="Boarding Address" />
+                <FormControl className="d-xl m-4 col-1">
+                    <InputLabel id="genderLbl">Preffered Language of Instructions</InputLabel>
+                    <Select
+                        labelId="genderLbl"
+                        id="gender"
+                        value="Male"
+                    >
+                        <MenuItem value="african">IsiXhosa</MenuItem>
+                        <MenuItem value="white">English</MenuItem>
+                        <MenuItem value="indian">IsiZulu</MenuItem>
+                        <MenuItem value="colourde">Afrikaans</MenuItem>
+                        <MenuItem value="colourde">Other</MenuItem>
+                    </Select>
+                </FormControl>
+                <FormControl className="d-xl m-4 col-1">
+                    <InputLabel id="genderLbl">Mode of transport to school</InputLabel>
+                    <Select
+                        labelId="genderLbl"
+                        id="gender"
+                        value="Male"
+                    >
+                        <MenuItem value="african">Public</MenuItem>
+                        <MenuItem value="white">Cab</MenuItem>
+                        <MenuItem value="indian">Private</MenuItem>
+                        <MenuItem value="colourde">Walk</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField className="d-xl m-4 col-2" label="Number of children in the family" />
+                <TextField className="d-xl m-4 col-2" label="Position in the mother's side" />
+                <TextField className="d-xl m-4 col-2" label="Position in the father's side" />
+                <TextField className="d-xl m-4 col-2" label="Email Address" />
+                <TextField className="d-xl m-4 col-2" label="Contact Number" />
+                <TextField className="d-xl m-4 col-2" label="Contact Number 2" />
+                <TextField className="d-xl m-4 col-4" label="Home Address" />
+                <FormControl className="d-xl m-4 col-1">
+                    <InputLabel id="genderLbl">Any deceased parent?</InputLabel>
+                    <Select
+                        labelId="genderLbl"
+                        id="gender"
+                        value="Male"
+                    >
+                        <MenuItem value="african">Mather</MenuItem>
+                        <MenuItem value="white">Father</MenuItem>
+                        <MenuItem value="indian">Both</MenuItem>
+                        <MenuItem value="colourde">None</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField className="d-xl m-4 col-2" label="Religion/Demomination" />
+                <FormControl className="d-xl m-4 col-1">
+                    <InputLabel id="genderLbl">Sport?</InputLabel>
+                    <Select
+                        labelId="genderLbl"
+                        id="gender"
+                        value="Male"
+                    >
+                        <MenuItem value="african">None</MenuItem>
+                        <MenuItem value="white">Soccer</MenuItem>
+                        <MenuItem value="indian">Rugby</MenuItem>
+                        <MenuItem value="colourde">Cricket</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField className="d-xl m-4 col-2" label="Pronvincial honours" />
+                
+                <TextField className="d-xl m-4 col-2" label="Previous School" />
+                <FormControl className="d-xl m-4 col-1">
+                    <InputLabel id="genderLbl">Province?</InputLabel>
+                    <Select
+                        labelId="genderLbl"
+                        id="gender"
+                        value="Male"
+                    >
+                        <MenuItem value="african">Eastern Cape</MenuItem>
+                        <MenuItem value="white">Gauteng</MenuItem>
+                        <MenuItem value="indian">Western Cape</MenuItem>
+                        <MenuItem value="colourde">Free State</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField className="d-xl m-4 col-2" label="Previous Country" />
+                <InputLabel id="genderLbl">Prevously enrolled in the province?</InputLabel>
+                <RadioGroup aria-label="gender" name="gender1" value="Never enrolled">
+                    <FormControlLabel value="female" control={<Radio />} label="Previously enrolled" />
+                </RadioGroup>
+                <TextField className="d-xl m-4 col-2" label="Previous School Address" />
+
+                <div className="input-group m-4 col-2">
+                    <div className="custom-file">
+                        <input type="file" className="custom-file-input d-xl" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01"/>
+                        <label className="custom-file-label" htmlFor="inputGroupFile01">Choose your image</label>
                     </div>
                 </div>
             </form>
